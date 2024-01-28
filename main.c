1440f80ed87091cfb3c67fa7337fdd6006aa01cd
Dmitri2401 <dimondriver@gmail.com>
2024-01-29 00:24:09 +0300

    ...

--- main.c
+++ main.c
@@ -172,11 +172,42 @@ main()
 */
 
 
+/*
 
+// not mine!
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
     printf("%d", EOF);
     return 0;
+}
+*/
+
+
+/*
+#include <stdio.h>
+
+// count characters in input; 1st version
+main()
+{
+    long nc;
+
+    nc = 0;
+    while (getchar() != EOF)
+    ++nc;
+    printf("%ld\n", nc);
+}
+*/
+
+#include <stdio.h>
+
+// count characters in input; 2nd version
+main()
+{
+    double nc;
+
+    for (nc = 0; getchar() != EOF; ++nc)
+    ;
+    printf("%.0f\n", nc);
 }
