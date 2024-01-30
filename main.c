9cfddace1acc44537ad0da254734f1012c928efc
Dmitri2401 <dimondriver@gmail.com>
2024-01-30 22:05:27 +0300

    ...

--- main.c
+++ main.c
@@ -61,10 +61,49 @@ int main()
 */
 
 
+/*
+#include <stdio.h>
+
+int main()
+{
+    double x = 5.2, y = 2.5;
+
+    double mult = x * y;
+    double add = x + y;
+    double div = x / y;
+    double sub = x - y;
+
+    printf("mult: %f\n", mult);
+    printf("add: %f\n", add);
+    printf("div: %f\n", div);
+    printf("sub: %f\n", sub);
+
+}
+*/
+
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int x = 11, y = 3;
+    
+    int div = x / y;
+    int mod = x % y;
+
+    printf("div: %d\n", div);
+    printf("mod: %d\n", mod);
+}
+*/
 
 #include <stdio.h>
 
 int main()
 {
-    
+    int x = 11;
+
+    int mod = x % 2;
+
+    printf("mod: %d\n", mod);
 }
