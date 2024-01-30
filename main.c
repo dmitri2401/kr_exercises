1278b14cc46d65f1e8b94281be3a4fb2c6f087aa
Dmitri2401 <dimondriver@gmail.com>
2024-01-30 18:55:34 +0300

    ...

---
+++ main.c
@@ -0,0 +1,70 @@
+/*
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World\n");
+    printf("Line 2!\n");
+    printf("Line 3!\n");
+}
+*/
+
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int speed = 0;
+    int time = 0;
+    printf("Speed: ");
+    scanf("%d", &speed);
+    printf("Time: ");
+    scanf("%d", &time);
+
+    int distance = speed * time;
+
+    printf("dist: %d\n", distance);
+}
+*/
+
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    float x = 5.2;
+    double y = -9.8;
+
+    printf("x: ");
+    scanf("%f", &x);
+    printf("x: %f\n", x);
+
+    printf("y: ");
+    scanf("%lf", &y);
+    printf("y: %f\n", y);
+}
+*/
+
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    printf("c: ");
+    scanf("%c", &c);
+    printf("c: %c\n", c);
+    printf("c: %d\n", c);
+}
+*/
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    
+}
