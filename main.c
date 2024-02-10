feaedf0c20746c5748b994fc8ab60f285d8c27aa
Dmitri2401 <dimondriver@gmail.com>
2024-02-10 18:21:00 +0300

    ...

--- main.c
+++ main.c
@@ -308,3 +308,56 @@ int main()
     
 }
 */
+
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    
+    for (int i = 0; i < 5; i++)
+        printf("i: %d\n", i);
+
+}
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    double initial = 0, step = 0, stop = 0;
+
+    do
+    {
+        printf("Initial (m): ");
+        scanf("%lf", &initial);
+        if (initial < 0) printf("Must be >= 0!\n");
+    }
+        while (initial < 0);
+
+    do
+    {
+        printf("Step (m): ");
+        scanf("%lf", &step);
+        if (step <= 0) printf("Must be > 0!\n");
+    }
+        while (step <= 0);
+
+    do
+    {
+        printf("Stop (m): ");
+        scanf("%lf", &stop);
+        if (stop < 0) printf("Must be >= 0!\n");
+    }
+        while (stop < 0);
+
+        printf("\n");
+        for (double conv = initial;
+             conv <= stop;
+             conv += step)
+    {
+        printf("%f %f\n", conv, conv * 3.28084);
+    }
+}
